f24f42265f38cd24523e33418cf132b381d25782
Mona LisaHeyClos <thiscarlos@gmail.com>
2019-09-09 16:50:51 -0700

    crud endpoints added

--- server.js
+++ server.js
@@ -6,4 +6,29 @@ const server = express();
 
 server.use(express.json());
 
+server.get('/', (req,res) => {
+    res.status(200).send('Welcome!')
+})
+
+server.get('/accounts', (req, res) => {
+    db('accounts')
+        .then(accounts => {
+            res.status(200).json(accounts);
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        })
+});
+
+server.get('/accounts/:id', (req, res) => {
+    db('accounts').where({ id: req.params.id })
+    .first()
+    .then(account => {
+        res.status(200).json(account);
+    })
+    .catch(error => {
+        res.status(500).json(error);
+    })
+})
+
 module.exports = server;
